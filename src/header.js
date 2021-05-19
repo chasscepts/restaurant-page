807fd87fc123febab5a52ff069c672ebffc3210a
chasscepts <francischassobetta@gmail.com>
2021-05-19 19:45:05 +0100

    Implement header

---
+++ src/header.js
@@ -0,0 +1,49 @@
+let listener;
+
+const handleClick = (() => {
+  let activeHeader;
+
+  return (header, page) => {
+    if (activeHeader) {
+      activeHeader.classList.remove('active');
+    }
+    activeHeader = header;
+    activeHeader.classList.add('active');
+    if (listener) {
+      listener(page);
+    }
+  };
+})();
+
+const elementSetup = (() => {
+  const element = document.createElement('header');
+  element.classList.add('main-header');
+  const nav = document.createElement('nav');
+  element.append(nav);
+
+  let homeHeader;
+
+  ['Home', 'Menu', 'Contact'].forEach((item) => {
+    const btn = document.createElement('button');
+    btn.classList.add('tab-header');
+    btn.textContent = item;
+    const page = item.toLowerCase();
+    btn.addEventListener('click', () => handleClick(btn, page));
+    if (!homeHeader) {
+      homeHeader = btn;
+    }
+    nav.append(btn);
+  });
+
+  return { element, homeHeader };
+})();
+
+const header = {
+  element: elementSetup.element,
+  setListener(callback) {
+    listener = callback;
+    handleClick(elementSetup.homeHeader, 'home');
+  },
+};
+
+export default header;
---
+++ src/tab_ids.js
@@ -0,0 +1,12 @@
+const tabIds = {
+  home: 'home-tab-item',
+  contact: 'contact-tab-item',
+  menu: 'menu-tab-item',
+};
+
+const headerClass = 'tab-header';
+
+export {
+  tabIds,
+  headerClass,
+};
